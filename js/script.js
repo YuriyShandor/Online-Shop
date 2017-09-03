e2e6bb3f823df80dce4235a7a5431167c87cb163
Yuriy Shandor <yuriyshandor@gmail.com>
2017-09-03 13:59:01 +0300

    Improve Mod_03

--- js/script.js
+++ js/script.js
@@ -4,11 +4,18 @@ $(document).ready(function(){
   $('select').niceSelect();
   });
 
+  $('.banner-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: $(".banner-slider__nav_right"),
+    prevArrow: $(".banner-slider__nav_left"),
+  });
+
   $(".gr-but").click(function(){
     $(".gr-but").removeClass("bl-but");
     $(this).addClass("bl-but");
     /*var index = $(this).index()+1;
     $(".content").removeClass("show");
     $(".content:nth-child("+ index +")").addClass("show");*/
-    });
   });
+});
